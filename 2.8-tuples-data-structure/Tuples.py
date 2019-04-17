9a6b29aefe9b961406acbacea3340e3eba1827d3
Kim Pham <pham.kim.a@gmail.com>
2019-04-17 13:11:20 -0500

    Section 2.8 Tuples: Done.

---
+++ 2.8-tuples-data-structure/Tuples.py
@@ -0,0 +1,23 @@
+# tuples are a sequence and are immutable (cannot delete or update items)
+
+tup = ("oranges", "apples", "bananas")
+
+# get first item of tuple
+tup[0]
+
+# get first two items of tuple
+tup[0:2]
+
+# can add two tuples together
+tup2 = (12, 14)
+tup + tup2
+
+# delete entire tuple
+del tup
+
+#length of tuple
+len(tup2)
+
+#multiply
+tup3 = ("HA")
+tup3 * 4
