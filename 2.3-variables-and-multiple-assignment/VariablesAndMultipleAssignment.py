a76833dba7c29201035b98f0b5fc5adb3da61ebd
Kim Pham <pham.kim.a@gmail.com>
2019-04-16 22:19:10 -0500

    Section 2.3 Variables & Multiple Assignment: Done.

---
+++ 2.3-variables-and-multiple-assignment/VariablesAndMultipleAssignment.py
@@ -0,0 +1,15 @@
+# variables are memory location to store values
+
+# variable name equals value
+age = 20
+sentence = "My name is Kim"
+
+# multiple assignment
+# assign each variable a different value
+tommy, samba, lili = 7, 8, 4
+
+# assign each variable the same value
+tommy = samba = lili = "dog"
+
+# assign two variables (name & age) in one line
+name, age = "Kim", 29
