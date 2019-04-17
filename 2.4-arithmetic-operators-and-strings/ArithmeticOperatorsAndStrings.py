71439999008a3ffed0456cb48256e380ed43359d
Kim Pham <pham.kim.a@gmail.com>
2019-04-16 22:46:04 -0500

    Section 2.4 Arithmetic Operators and Strings: Done.

---
+++ 2.4-arithmetic-operators-and-strings/ArithmeticOperatorsAndStrings.py
@@ -0,0 +1,47 @@
+# arithmetic operators and how they apply to variables
+
+age1 = 12
+age2 = 18
+
+# sum
+age1 + age2
+
+# subtraction
+age1 - age2
+
+# multiplication
+age1 * age2
+
+# division
+age1 / age2
+
+# modulus gives remainder
+age2 % age1
+
+
+# strings
+sent1 = "Today was a beautiful day"
+
+firstName = "Kim"
+lastName = "Pham"
+
+# concatinate strings
+firstName + " " + lastName
+
+# multiply string by integer
+"HA" * 10
+
+# CANNOT subtract, divide, or modulus strings
+
+
+# every character in a string has an index
+sentence = "Kim is learning Python"
+# splicing (taking string and getting a specific segment from it)
+sentence[0]
+
+# python doesn't include the ending position
+sentence[0:3]
+sentence[:3]
+
+# get everything but last two characters
+sentence[:-2]
