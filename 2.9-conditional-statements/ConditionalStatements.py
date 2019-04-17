25d2d2eaff3feb4f8943019171f28034ce9e80d6
Kim Pham <pham.kim.a@gmail.com>
2019-04-17 13:38:45 -0500

    Section 2.9 Conditional Statement: Done.

---
+++ 2.9-conditional-statements/ConditionalStatements.py
@@ -0,0 +1,20 @@
+# conditional statements, if this else that
+
+if (5 > 3):
+    print("Hello")
+else:
+    print("Goodbye")
+
+# six relational operators: > < >= <= == !=
+
+age = 32
+
+if (age < 18):
+    print("You are young.")
+elif (age >= 18 and age < 30):
+    print("You are an adult.")
+else:
+    print("You are old.")
+
+# and makes sure that both conditions are true
+# or makes sure at least one condition is true
