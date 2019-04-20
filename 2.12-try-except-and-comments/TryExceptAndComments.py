5fc70b73ff6d09652a8d726888191ae3bf501f3e
Kim Pham <pham.kim.a@gmail.com>
2019-04-19 19:09:01 -0500

    Section 2.12 Try-Except and Comments: Done.

---
+++ 2.12-try-except-and-comments/TryExceptAndComments.py
@@ -0,0 +1,20 @@
+# try and except case
+# used when you're running a piece of code but not sure if result will be there or if it will function correctly
+# if code in try does not work, then it will default to except case
+
+try:
+    if name > 3:
+        print("hello")
+except:
+    print("there's something wrong with your code")
+
+# commenting
+
+# single line commenting
+# this is a comment
+
+# multiple line commenting
+"""
+this is a comment
+on multiple lines
+"""
